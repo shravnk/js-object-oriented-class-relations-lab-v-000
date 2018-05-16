2cd26c8219c2f7a76cea2d870d3956f2fca43146
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 03:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,13 +55,13 @@ class Trip {
 
   driver() {
     return store.drivers.find(driver => {
-      return driver.tripId === this.id
+      return driver.id === this.driverId
     })
   }
 
   passenger() {
     return store.passengers.filter(passenger => {
-      return passenger.tripId === this.id
+      return passenger.id === this.passengerId
     })
   }
 }
