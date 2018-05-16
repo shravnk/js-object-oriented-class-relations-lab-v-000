ea46909c0a566c65aec62ff28e7993886e0d84e9
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 03:52:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,73 @@
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+let store = {drivers: [], passengers: [], trips: []}
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.driverId === this.id
+    })
+  }
+
+  passengers() {
+    return this.trips().map(trip => {
+      return trip.passenger()
+    })
+  }
+
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter(trip => {
+      return trip.passengerId === this.id
+    })
+  }
+  drivers() {
+    return this.trips().map(function(trip) {
+      return trip.driver()
+    })
+  }
+
+  // drivers() {
+  //   return store.drivers.filter(driver => {
+  //     return driver.passengerId === this.id
+  //   })
+  // }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    store.trips.push(this)
+  }
+
+  driver() {
+    return store.drivers.find(driver => {
+      return driver.id === this.driverId
+    })
+  }
+
+  passenger() {
+    return store.passengers.find(passenger => {
+      return passenger.id === this.passengerId
+    })
+  }
+}
