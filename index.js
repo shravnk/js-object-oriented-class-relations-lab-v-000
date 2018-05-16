8e8e8defbe620ae7f70f3b31da2c9e4f59232a8c
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-16 03:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-class Driver {
+driverId = 0
+passengerId = 0
+tripId = 0
 
+class Driver {
+  constructor(name) {
+    this.id = ++driverId
+    this.name = name
+  }
 }
 
 class Passenger {
-
+  constructor(name) {
+    this.id = ++passengerId
+    this.name = name
+  }
 }
 
 class Trip {
-
+  constructor(name, driver, passenger) {
+    this.id = ++tripId
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+  }
 }
